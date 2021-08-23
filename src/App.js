d760a494731768ca34cc6883085b4180f8e2fabf
Szymon Gos <szymonngos@gmail.com>
2021-08-23 16:32:11 +0100

    add build to .gitignore, clean App.js/main.sass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Button from './components/Button';
 
 export default function App(){
     return (
         <div>
-            <h1>Welcome to React-webpack-sass-boilerplate</h1>
-            <br />
-            <h2> import _test file sass</h2>
-            <Button />
+            <h1>Welcome to React-webpack-babel-sass-boilerplate</h1>
         </div>
     );
 }
