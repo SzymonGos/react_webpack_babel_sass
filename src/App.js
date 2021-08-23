d253a8afb9efbc63f6a0c8875e73976d78dfac34
Szymon Gos <szymonngos@gmail.com>
2021-08-23 16:23:59 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './components/Button';
+
+export default function App(){
+    return (
+        <div>
+            <h1>Welcome to React-webpack-sass-boilerplate</h1>
+            <br />
+            <h2> import _test file sass</h2>
+            <Button />
+        </div>
+    );
+}
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    output: {
+        path: path.join(__dirname, '/build'),
+        filename: 'index.bundle.js',
+    },
+    devServer: {
+        port: 8080,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(sass|scss)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body',
+          }),
+    ],
+};
